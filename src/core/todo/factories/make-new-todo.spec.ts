73c8aa482adccd187bdaa92e8abe0209522de107
cleyton vinicius <cleytonterceiro@outlook.com>
2025-05-19 15:51:22 -0300

    feat: add makeNewTodo function and corresponding unit tests

---
+++ src/core/todo/factories/make-new-todo.spec.ts
@@ -0,0 +1,28 @@
+import { describe, it } from "vitest";
+import { makeNewTodo } from "./make-new-todo";
+
+describe("MakeNewTodo (unit)", () => {
+  describe("create", () => {
+    it("should be return a valid todo", () => {
+      //AAA -> Arrange, Act, Assert
+
+      // Arrange -> Criar as coisas que preciso
+      const expectedTodo = {
+        // id: 'any-id',
+        id: expect.any(String),
+        description: "novo todo",
+        // createdAt: new Date().toISOString()
+        createdAt: expect.any(String),
+      };
+
+      //Act
+      const newTodo = makeNewTodo("novo todo");
+
+      // Assert
+      // toBe ===
+      // toEqual | toStrictEqual
+      expect(newTodo.description).toBe(expectedTodo.description);
+      expect(newTodo).toStrictEqual(expectedTodo);
+    });
+  });
+});
