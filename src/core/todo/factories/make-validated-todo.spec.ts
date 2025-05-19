660d860180f7652e36faf63574d5fef5647698a5
cleyton vinicius <cleytonterceiro@outlook.com>
2025-05-19 20:19:19 -0300

    feat: add unit tests for MakeValidatedTodo functionality(3)

---
+++ src/core/todo/factories/make-validated-todo.spec.ts
@@ -0,0 +1,9 @@
+describe("MakeValidatedTodo (unit)", () => {
+  it("should be able to call sanitizeStr with the correct value", () => {});
+
+  it("should be able to call validateAllDescription with the return of sanitizeStr", () => {});
+
+  it("should be able to call makeNewTodo if validatedDescription returns success", () => {});
+
+  it("should return a list of errors if validatedDescription fails", () => {});
+});
